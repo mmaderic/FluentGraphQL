9d618cb645443c82e1bfdaaf9e2fa623eed680df
Mateo Mađerić <mateo.maderic@gmail.com>
2020-08-18 12:42:21 +0200

    removed TPrimaryKey generic argument

--- FluentGraphQL.Abstractions/Builder/IGraphQLEntity.cs
+++ FluentGraphQL.Abstractions/Builder/IGraphQLEntity.cs
@@ -19,9 +19,4 @@ namespace FluentGraphQL.Builder.Abstractions
     public interface IGraphQLEntity
     {
     }
-
-    public interface IGraphQLEntity<TPrimaryKey> : IGraphQLEntity
-    {
-        TPrimaryKey Id { get; set; }
-    }
 }

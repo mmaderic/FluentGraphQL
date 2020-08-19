22444963e9646edb6f39a71ce92425525dd9c93e
Mateo Mađerić <mateo.maderic@gmail.com>
2020-08-20 00:29:05 +0200

    selected query implementation

---
+++ FluentGraphQL.Abstractions/Builder/IGraphQLSelectedQuery.cs
@@ -0,0 +1,35 @@
+﻿/*
+    MIT License
+
+    Copyright (c) 2020 Mateo Mađerić
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+*/
+
+using System;
+
+namespace FluentGraphQL.Builder.Abstractions
+{
+    public interface IGraphQLSelectedQuery<TEntity, TResult>
+    {
+        Func<TEntity, TResult> Selector { get; }
+    }
+
+    public interface IGraphQLStandardSelectedQuery<TEntity, TResult> : IGraphQLSelectedQuery<TEntity, TResult>, IGraphQLStandardQuery<TResult>
+    {
+        IGraphQLStandardQuery<TEntity> Cast();
+    }
+
+    public interface IGraphQLSingleSelectedQuery<TEntity, TResult> : IGraphQLSelectedQuery<TEntity, TResult>, IGraphQLSingleQuery<TResult>
+    {
+        IGraphQLSingleQuery<TEntity> Cast();
+    }
+}
---
+++ FluentGraphQL.Builder/Constructs/GraphQLSelectedQuery.cs
@@ -0,0 +1,43 @@
+﻿/*
+    MIT License
+
+    Copyright (c) 2020 Mateo Mađerić
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+*/
+
+using FluentGraphQL.Builder.Abstractions;
+using System;
+
+namespace FluentGraphQL.Builder.Constructs
+{
+    public class GraphQLSelectedQuery<TEntity, TResult> : GraphQLQuery<TEntity>, IGraphQLStandardSelectedQuery<TEntity, TResult>, IGraphQLSingleSelectedQuery<TEntity, TResult>
+           where TEntity : IGraphQLEntity
+    {
+        public Func<TEntity, TResult> Selector { get; }
+
+        public GraphQLSelectedQuery(IGraphQLHeaderNode graphQLHeaderNode, IGraphQLSelectNode graphQLSelectNode, Func<TEntity, TResult> selector)
+            : base(graphQLHeaderNode, graphQLSelectNode)
+        {
+            Selector = selector;
+        }
+
+        IGraphQLStandardQuery<TEntity> IGraphQLStandardSelectedQuery<TEntity, TResult>.Cast()
+        {
+            return this;
+        }
+
+        IGraphQLSingleQuery<TEntity> IGraphQLSingleSelectedQuery<TEntity, TResult>.Cast()
+        {
+            return this;
+        }
+    }
+}
